502b3039224d7d69eb92f61b083f0e6df1ae26cd
luojianhe1992 <luojianhe1992@gmail.com>
2018-02-15 17:56:03 -0500

    source code and POM file

---
+++ src/main/java/com/ibm/jenkins_test/App.java
@@ -0,0 +1,12 @@
+package com.ibm.jenkins_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hi, I am Jianhe Luo......");
+	}
+}
